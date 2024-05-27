3ee6e1fa6672b3a979fd3f36730b433baecf62d2
Pierre Chevaillier <pierre.chevaillier@enib.fr>
2024-05-27 21:29:44 +0200

    add misc

---
+++ misc/empty.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# context: source file template for python (personal usage)
+# description: header template for python code files
+# copyright (c) 2024 Pierre Chevaillier. All rights reserved.
+# ------------------------------------------------------------------------------
+# usage: python <thisFileName> - import XXX from <thisFileName>
+# dependencies: python 3 (see import statements)
+# used with:
+#   - python 3.8.17 on macOS 13.6
+#   - python 3.x on
+# ------------------------------------------------------------------------------
+# creation: 27-may-2024 pchevaillier@gmail.com
+# revision:
+# ------------------------------------------------------------------------------
+# comments:
+# -
+# warnings:
+# - still under development - not fully tested
+# - for personal usage only
+# todos:
+#  - test it and then test it again
+# ==============================================================================
+
+# end of file
+# ==============================================================================
